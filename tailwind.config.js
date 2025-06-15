0ec0b357083fb8576087db3a0b3ec0f4c471ca15
Devs <devangsuthar24@gmail.com>
2025-06-15 22:39:47 +0530

    'banner-updated'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,css}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        aileron: ["Aileron", "sans-serif"],
-      },
-    },
-  },
-  plugins: [],
+	content: ['./src/**/*.{html,js,css}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				aileron: ['Aileron', 'sans-serif'],
+			},
+			colors: {
+				primary: '#F56C24',
+				theme_color: '#19458C',
+			},
+		},
+	},
+	plugins: [],
 };
